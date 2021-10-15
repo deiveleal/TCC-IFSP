9884825609b1f9b3132f4702bef2f4fe73092f8c
Deive <deive.leal@gmail.com>
2021-10-15 09:09:58 -0300

    Adiciona sql

---
+++ datasets/create_table_covid19_campanha_vacinacao_raw.sql
@@ -0,0 +1,12 @@
+CREATE TABLE covid19_campanha_vacinacao (
+  paciente_endereco_uf varchar(3),
+  paciente_endereco_municipio varchar(52),
+  paciente_raca_cor varchar(21),
+  vacina_dataAplicacao varchar(25),
+  vacina_nome varchar(70),
+  paciente_data_nascimentovarchar(12),
+  vacina_descricao_dose varchar(12),
+  estabelecimento_uf varchar(3),
+  vacina_categoria_nome varchar(25),
+  paciente_idade varchar(3)
+)
